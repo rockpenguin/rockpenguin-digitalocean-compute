494659225053c1a487798de46c74c76ec545bf69
Dave Heebner <dave@rockpenguin.com>
2026-05-14 13:08:34 -0400

    Adding DNS naming and outputs

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 ###############################################################################
+# DOMAINS
+# Get the list of DO hosted domains so we can double check Droplets DNS
+###############################################################################
+data "digitalocean_domains" "all" {}
+
+###############################################################################
 # DROPLETS
 ###############################################################################
-
 resource "digitalocean_droplet" "droplet" {
   for_each = var.droplets
 
@@ -9,23 +14,37 @@ resource "digitalocean_droplet" "droplet" {
   name    = each.value.name
   region  = each.value.region
   size    = each.value.size
-  backups = each.value.backups
+  backups = each.value.backups_enabled
   dynamic "backup_policy" {
     # If backups = false then make this an empty map
-    for_each = each.value.backups ? each.value.backup_policy : {}
+    for_each = each.value.backups_enabled ? each.value.backup_policy : {}
     content {
       plan    = each.value.backup_policy.plan
       weekday = each.value.backup_policy.weekday
       hour    = each.value.backup_policy.hour
     }
   }
-  droplet_agent = each.value.droplet_agent
-  monitoring = each.value.monitoring
-  ssh_keys = each.value.ssh_keys
-  tags = each.value.tags
-  user_data = each.value.user_data
-  volume_ids = each.value.volume_ids
-  vpc_uuid = each.value.vpc_uuid
+  droplet_agent     = each.value.droplet_agent
+  monitoring        = each.value.monitoring
+  ssh_keys          = each.value.ssh_keys
+  tags              = each.value.tags
+  user_data         = each.value.user_data
+  volume_ids        = each.value.volume_ids
+  vpc_uuid          = each.value.vpc_uuid
   public_networking = each.value.public_networking
-  ipv6 = each.value.ipv6
+  ipv6              = each.value.ipv6
+}
+
+resource "digitalocean_record" "droplet_dns_a" {
+  # ensure DNS naming is enabled and that the domain is valid e.g. ! misspelled
+  for_each = {
+    for id, droplets in var.droplets : id => droplets
+      if droplets.dns_enabled && contains(data.digitalocean_domains.all.domains[*].name, droplets.dns_domain)
+  }
+
+  domain = each.value.dns_domain
+  name = each.value.name
+  type = "A"
+  ttl = 600
+  value = digitalocean_droplet.droplet[each.key].ipv4_address
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,29 @@
 variable "droplets" {
   description = "Droplets definition"
   type = map(object({
-    image   = string
-    name    = string
-    region  = string
-    size    = string
-    backups = optional(bool, false)
-    backup_policy = optional(object({
+    image           = string
+    name            = string
+    region          = string
+    size            = string
+
+    backups_enabled = optional(bool, false)
+    backup_policy   = optional(object({
       plan    = string
       weekday = string
       hour    = number
     }))
-    droplet_agent = optional(bool, true)
-    monitoring = optional(bool, false)
-    ssh_keys = list(string)
-    tags = optional(list(string))
-    user_data = optional(string)
-    volume_ids = optional(list(string))
-    vpc_uuid = string
+
+    droplet_agent     = optional(bool, true)
+    monitoring        = optional(bool, false)
+    ssh_keys          = list(string)
+    tags              = optional(list(string))
+    user_data         = optional(string)
+    volume_ids        = optional(list(string))
+    vpc_uuid          = optional(string)
     public_networking = optional(bool)
-    ipv6 = optional(bool, false)
+    dns_enabled       = optional(bool, false)
+    dns_domain        = optional(string)
+    ipv6              = optional(bool, false)
   }))
   default = {}
 }
